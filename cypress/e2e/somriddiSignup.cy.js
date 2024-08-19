196608943ed7635c36a0357a8b09189939f6eeb9
rijviahmedrahat <angelnishat4746@gmail.com>
2024-08-19 09:55:10 +0600

    only sign up updates

--- cypress/e2e/somriddiSignup.cy.js
+++ cypress/e2e/somriddiSignup.cy.js
@@ -2,10 +2,9 @@ describe('dropdown handel',()=>{
     it('user can handel dropsown',()=>{
         cy.visit("https://somriddhi.eksheba.gov.bd/");
         cy.contains("Sign Up").wait(1000).click()
-        cy.get('#name').should('have.attr', 'placeholder', 'Enter name').type(userID_Alpha());
-        cy.get('#phone').should('have.attr', 'placeholder', '01xxxxxxxxx').type(userID_Alpha()+'@gmail.com');
+        cy.get('#name').should('have.attr', 'placeholder', 'Enter name').type('name');
+        cy.get('#phone').should('have.attr', 'placeholder', '01xxxxxxxxx').type('abc1@gmail.com');
         cy.contains("Register").wait(1000).click()
-        cy.get('')
 })
 
 })
