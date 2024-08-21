5862e6788074c39c820855c6a01232b14b103427
rijviahmedrahat <angelnishat4746@gmail.com>
2024-08-21 14:46:06 +0600

    done

--- cypress/e2e/lamsUserApprove.cy.js
+++ cypress/e2e/lamsUserApprove.cy.js
@@ -15,10 +15,19 @@ describe('user_registration', () => {
         cy.contains('User').click()
 
 
-      
-        cy.contains('Approve').click();
+        // Find all div elements with the role of "button" that contain "Approve" as the label
+        cy.get('div[role="button"]').each(($el) => {
+        // Inside each button, check if it contains a span with the text "Approve"
+        cy.wrap($el).find('span.MuiChip-label').then(($span) => {
+        if ($span.text().trim() === 'Approve') {
+        // Scroll the element into view and click it
+        cy.wrap($el).scrollIntoView().click({ force: true });
+        cy.get('button').contains('Yes').click({ force: true });
+              }
 
-       
+            });
+          });
+      
 
     
 
